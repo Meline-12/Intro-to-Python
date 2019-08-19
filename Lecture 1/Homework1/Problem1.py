fa146bd7fab8c6064b34e6e3159abe0e0d97d8c5
Meline-12 <ayvazyanmeline@gmail.com>
2019-08-19 22:17:27 +0400

    c

---
+++ Lecture 1/Homework1/Problem1.py
@@ -0,0 +1,22 @@
+project="cake"
+print(project)
+difficulty=5
+print(difficulty)
+ingredients=["flour", "butter", "sugar", "eggs", "cocoa powder", "baking powder"]
+print(ingredients)
+print("apples" in ingredients)
+print("butter" in ingredients)
+print("eggs" or "margarine" in ingredients)
+print("eggs" and "margarine" in ingredients)
+flour=175
+type(flour)
+butter=175
+type(butter)
+sugar=100
+type(sugar)
+eggs=2
+type(eggs)
+cocoa_powder="1ts"
+type(cocoa_powder)
+baking_powder=0.5
+type(baking_powder)
---
+++ Lecture 1/Homework1/Problem4.py
@@ -0,0 +1,6 @@
+number = int(input("Enter the number"))
+mod = number % 2
+if mod>0:
+    print("odd")
+else:
+    print("even")
